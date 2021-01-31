71f3e88fb1c9d2bb4a94220f64728d1ecc5f1a30
serafinpl <ser.burberibar@gmail.com>
2021-01-31 23:21:31 +0100

    Poprawki na persons

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,29 +1,11 @@
 import React from 'react';
 
-import './Person.css';
-
-import styled from 'styled-components';
+import classes from'./Person.module.css';
 
 
-const StyledDiv = styled.div`
 
-		width: 70%;
-		margin:15px auto;
-		border: 1px solid #eee;
-		box-shadow: 0 2px 4px #ddd;
-		padding: 15px;
-		text-align: center;
-		
 
-		p {
-			display: inline;
-			margin: 20px;
-		}
-		@media (min-width: 500px){
-			width: 450px;
-		}
 
-		`
 
 const Person = (props) => {
 
@@ -32,11 +14,11 @@ const Person = (props) => {
 
 	return(
 		
-		<StyledDiv>
+		<div className={classes.persons}>
 			<p onClick={props.funkcja}>Jestem {props.name} i mam {props.age} lat!</p>
 			<p>{props.children}</p>
 			<input type="text" onChange={props.zmiana} value={props.name} />
-		</StyledDiv>
+		</div>
 	)
 }
 
