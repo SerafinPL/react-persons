60df436516841f46ff9bfe300b85a97c95d47270
serafinpl <ser.burberibar@gmail.com>
2021-02-10 12:05:55 +0100

    useEffect in cockpit

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import classes from'./Cockpit.module.css';
 
 const Cockpit = (props) => {
+
+	useEffect(() => (
+		console.log('Cockpit.js useEffect')
+	));
 
 
 	let klasyParagrafu = [];//['colorRed', 'bold'].join(' '); // wynik "colorRed bold" jako string
