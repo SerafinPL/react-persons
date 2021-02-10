e08a5af3dceadb58d14241962e6f2b0d4a4f8902
serafinpl <ser.burberibar@gmail.com>
2021-02-10 20:25:57 +0100

    PureComponent

--- src/components/Persons/Persons-class.js
+++ src/components/Persons/Persons-class.js
@@ -1,31 +1,39 @@
-import React, {Component} from 'react';
+import React, {PureComponent} from 'react';
 import Person from './Person/Person';
 
 
-class Persons extends Component 	{
+class Persons extends PureComponent 	{
 
   static getDerivedStateFromProps(props, state){
     console.log('PERSONS.js getDerivedStateFromProps props: ' , props);
     return state;
   } 
 
-  // componentWillReceiveProps(props){
+  // componentWillReceiveProps(props){ 
   //   console.log('Persons.js componetWillReceiveProps', props);
-  // } WYCOFANY
+  //    WYCOFANY
+  // } 
 
   //  componentWillUpdate{
+    //WYCOFANY
+  // } 
 
-  // } WYCOFANY
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('PERSONS.js shouldComponentUpdate');
-    if (nextProps.persons !== this.props.persons){
-      return true;
-    } else {
-      return false;
-    }
-    // return tak lub nie update component
-  }
+  // Zamiast porównywać wszystkie props można użyć PureComponent który ma tą funkcję na standardzie
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log('PERSONS.js shouldComponentUpdate');
+  //   if (nextProps.persons !== this.props.persons || 
+  //       nextProps.changed !== this.props.changed ||
+  //       nextProps.clicked !== this.props.clicked
+  //     ){
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  //   // return tak lub nie update component
+  // }
+
+
 
   getSnapshotBeforeUpdate(prevProps, prevState){
     console.log('PERSONS.js getSnapshotBeforeUpdate');
